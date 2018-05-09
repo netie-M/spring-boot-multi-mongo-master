7da454bbd1c7ff7ae112f363d419f8fdf65fdee3
netie-M <otheroperation@163.com>
2018-05-09 10:38:00 +0800

    Update PospServiceImpl.java

--- src/main/java/com/example/PospServiceImpl.java
+++ src/main/java/com/example/PospServiceImpl.java
@@ -1,81 +1,77 @@
 package com.quark.cobra.posp.service.impl;
 
-import com.quark.cobra.posp.domain.CreatAccBindBo;
-import com.quark.cobra.posp.enums.ApplicationCode;
-import com.quark.cobra.posp.enums.BankCardType;
-import com.quark.cobra.posp.enums.BuCode;
-import com.quark.cobra.posp.enums.CheckType;
-import com.quark.cobra.posp.enums.GatewayType;
-import com.quark.cobra.posp.enums.InterfaceSync;
-import com.quark.cobra.posp.enums.OperAccountType;
-import com.quark.cobra.posp.enums.UserRole;
+import com.alibaba.fastjson.JSON;
+import com.alibaba.fastjson.JSONObject;
+import com.quark.cobra.posp.domain.BaseReqBo;
+import com.quark.cobra.posp.domain.BaseRspBo;
 import com.quark.cobra.posp.service.IPospService;
-import org.apache.commons.lang3.RandomUtils;
-import org.joda.time.DateTime;
+import lombok.Cleanup;
+import lombok.extern.slf4j.Slf4j;
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.beans.factory.annotation.Value;
+import org.springframework.http.HttpEntity;
+import org.springframework.http.HttpHeaders;
+import org.springframework.http.MediaType;
 import org.springframework.http.ResponseEntity;
 import org.springframework.stereotype.Service;
+import org.springframework.util.LinkedMultiValueMap;
+import org.springframework.util.MultiValueMap;
+import org.springframework.util.StringUtils;
 import org.springframework.web.client.RestTemplate;
 
-import javax.annotation.Resource;
+import java.io.ByteArrayInputStream;
+import java.io.ByteArrayOutputStream;
+import java.io.IOException;
+import java.io.InputStream;
+import java.util.Arrays;
+import java.util.Map;
+import java.util.zip.GZIPInputStream;
 
 @Service
-public class PospServiceImpl implements IPospService{
-    @Resource(name="defaultRestTemplate")
+@Slf4j
+public class PospServiceImpl implements IPospService {
+    @Autowired
     private RestTemplate restTemplate;
+    @Value("${posp.server.api.url}")
+    private String pospApiUrl;
+    @Value("${posp.server.gate-way.url}")
+    private String pospGatewayUrl;
 
     @Override
-    public void creatAccBind() {
-        ResponseEntity<String> responseEntity = restTemplate.postForEntity(""
-            ,CreatAccBindBobuilder("","","","","","","","")
-            ,String.class);
-        System.out.println(responseEntity.getBody());
-
+    public BaseRspBo doPostPospApi(BaseReqBo baseReqBo){
+        String responseString = restTemplate.postForObject(pospApiUrl,baseReqBo,String.class);
+        return StringUtils.isEmpty(responseString)?null:JSON.parseObject(responseString,BaseRspBo.class);
     }
 
+    @Override
+    public String doPostPospGateWay(BaseReqBo baseReqBo) throws IOException {
+        HttpHeaders headers = new HttpHeaders();
+        headers.add("Content-Type",MediaType.APPLICATION_FORM_URLENCODED_VALUE);
+        headers.add("Accept", "text/plain;charset=utf-8");
+        headers.add("Accept-Encoding", "gzip");
+        headers.add("Content-Encoding", "UTF-8");
+        Map map = JSON.toJavaObject((JSONObject)JSON.toJSON(baseReqBo),Map.class);
+        MultiValueMap<String, String> params = new LinkedMultiValueMap<String, String>();
+        params.setAll(map);
+        HttpEntity< MultiValueMap<String,String>> requestEntity = new HttpEntity<MultiValueMap<String,String>>(params,headers);
+        ResponseEntity<byte[]> responseEntity = restTemplate.postForEntity(pospGatewayUrl,requestEntity,byte[].class);
+        return gunzip(responseEntity.getBody());
+    }
 
-    private CreatAccBindBo CreatAccBindBobuilder(String idName,String idNo,String bankCode,String bankName,String bankCardNo,String mobile,String returnUrl,String notifyUrl){
-        DateTime now = DateTime.now();
-        String interfaceId =  "QF-POSP-DEPO-CREATE-ACC-BIND-0001";
-        String merchantCode = "10" ;
-        BuCode buCode = BuCode.quark;
-        String operAccount = "C10";
-        ApplicationCode applicationCode = ApplicationCode.posp;
-        String tradeNo = new StringBuffer()
-            .append(merchantCode)
-            .append(buCode.getCode())
-            .append(applicationCode.getCode())
-            .append(interfaceId.replaceAll("[^0-9]",""))
-            .append(RandomUtils.nextInt(1,9999))
-            .toString();
-        return  CreatAccBindBo.builder()
-            .interfaceId(interfaceId)
-            .gatewayType(GatewayType.HenFeng)  //TODO
-            .merchantCode(merchantCode)
-            .buCode(buCode)
-            .applicationCode(applicationCode)
-            .interfaceSync(InterfaceSync.gateway)
-            .tradeNo(tradeNo)
-            .tradeDate(now.toString("yyyy-MM-dd"))
-            .tradeTime(now.toString("HH:mm:ss"))
-            .name(idName)
-            .certType("101")
-            .certNo(idNo)
-            .bankCode(bankCode)
-            .bankName(bankName)
-            .bankCardNo(bankCardNo)
-            .bankCardType(BankCardType.debit)
-            .reservedPhone(mobile)
-            .userRole(UserRole.borrower) //TODO
-            .operAccount(operAccount)
-            .operAccountType(OperAccountType.open)
-            .checkType(CheckType.four_factors)
-//            .authList(null)
-//            .authDate()
-//            .authAmount()
-            .returnUrl(returnUrl)
-            .notifyUrl(notifyUrl)
-//            .remark()
-            .build();
+    public String gunzip(byte[] bytes){
+        try {
+            @Cleanup ByteArrayInputStream in = new ByteArrayInputStream(bytes);
+            @Cleanup ByteArrayOutputStream out = new ByteArrayOutputStream();
+            @Cleanup GZIPInputStream gunzip = new GZIPInputStream(in);
+            byte[] buffer = new byte[256];
+            int n;
+            while ((n = gunzip.read(buffer))>= 0) {
+                out.write(buffer, 0, n);
+            }
+            return out.toString("utf-8");
+        } catch (IOException e) {
+            log.error("gunzip失败",e);
+            return "";
+        }
     }
 }
